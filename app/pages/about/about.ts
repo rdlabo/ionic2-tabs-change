cfa75f6dbeb3395f593f17d8e74a815c26bce418
榊原昌彦 <sakakibara@rdlabo.jp>
2016-09-19 16:03:41 +0900

    add loader in willenter. end console log.

--- app/pages/about/about.ts
+++ app/pages/about/about.ts
@@ -1,10 +1,30 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController,LoadingController } from 'ionic-angular';
 
 @Component({
   templateUrl: 'build/pages/about/about.html'
 })
 export class AboutPage {
-  constructor(public navCtrl: NavController) {
+  constructor(
+      public navCtrl: NavController,
+      public loadCtrl:LoadingController
+  ) {}
+
+  ionViewWillEnter()
+  {
+    let loader = this.loadCtrl.create();
+    loader.present();
+    loader.dismiss();
+    console.log("AboutPage ionViewWillEnter");
+  }
+
+  ionViewLoaded()
+  {
+    console.log("AboutPage ionViewLoaded");
+  }
+
+  ionViewWillLeave()
+  {
+    console.log("AboutPage ionViewWillLeave");
   }
 }
